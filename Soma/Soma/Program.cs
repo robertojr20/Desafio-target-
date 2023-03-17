df4a929807c5f63f385d2b20ce29fbdd1a10b6d5
Roberto Varela Dos Santos  Junior <83435209+robertojr20@users.noreply.github.com>
2023-03-17 17:03:54 -0300

    Add files via upload

---
+++ Soma/Soma/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ 1) Observe o trecho de código abaixo:
+
+ int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+
+{
+
+K = K + 1;
+
+SOMA = SOMA + K;
+
+}
+
+imprimir(SOMA);
+
+ Ao final do processamento, qual será o valor da variável SOMA?
+ Resposta : o valor da soma vai dar "91"
+ */
+
+
+using System;
+
+namespace soma // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int indice = 13, SOMA = 0, K = 0;
+
+            while (K < indice)
+            {
+                K = K + 1;
+
+                SOMA = SOMA + K;
+            }
+            Console.WriteLine(SOMA);
+        }
+    }
+}
